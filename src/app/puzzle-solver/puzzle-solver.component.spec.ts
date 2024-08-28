b0d9435b9b1042ceebe7f258fd6c4d2b6cc45e3e
Evan Palmer <evansp01@gmail.com>
2024-08-28 04:11:59 +0100

    solver test fixed

--- src/app/puzzle-solver/puzzle-solver.component.spec.ts
+++ src/app/puzzle-solver/puzzle-solver.component.spec.ts
@@ -2,33 +2,38 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PuzzleSolverComponent } from './puzzle-solver.component';
 import { AppModule } from '../app.module';
-import { PuzzleStateService } from '../core/puzzle-state.service';
 import { AcrFormatService } from '../core/acrformat.service';
 import { examplePuzzle } from '../testdata/puzzles';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('PuzzleSolverComponent', () => {
   let component: PuzzleSolverComponent;
   let fixture: ComponentFixture<PuzzleSolverComponent>;
+  const puzzle = new AcrFormatService().parseFile(examplePuzzle);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PuzzleSolverComponent],
       imports: [AppModule],
-      providers: [AppModule]
+      providers: [AppModule, {
+        provide: ActivatedRoute,
+        useValue: {
+          data: of({
+            puzzle: {
+              id: "test",
+              link: "test",
+              filename: "test",
+              puzzle: puzzle
+            }
+          })
+        }
+      }]
     })
       .compileComponents();
   });
 
   beforeEach(() => {
-    const puzzleStateService = TestBed.inject(PuzzleStateService);
-    const acrFormatService = TestBed.inject(AcrFormatService);
-    puzzleStateService.setPuzzle({
-      id: "test",
-      link: "test",
-      filename: "test",
-      puzzle: acrFormatService.parseFile(examplePuzzle)
-    });
-
     fixture = TestBed.createComponent(PuzzleSolverComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
